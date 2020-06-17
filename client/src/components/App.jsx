f18b8cebd0b7a879ba7a8f0c4a862d41cd3f468b
Tokki0401 <gyuhun95@gmail.com>
2020-06-17 16:56:52 -0700

    setup complete

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: []
+    }
+  }
+
+
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/public'),
+  },
+  module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
