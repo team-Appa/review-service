322a7a82fb8d6cc9f0ec19ed495bdb18cff77480
Tokki0401 <gyuhun95@gmail.com>
2020-06-29 17:25:41 -0700

    get request fixed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const db = require('../db/index.js');
 const Review = require('../db/comments.js');
@@ -8,6 +9,7 @@ const Review = require('../db/comments.js');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
@@ -17,7 +19,7 @@ app.get('/api/reviews', function(req, res) {
   Review.find({ id })
     .exec((err, result) => {
       if (err) { console.log('Error getting reviews', err); }
-      res.status(200).json(result);
+      res.status(200).json(result[0].reviews);
     });
 });
 
