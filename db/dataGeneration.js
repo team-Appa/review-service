eefbae3bb22db05d3f780dea437f6dda000d80da
Jing Chen <jchen0506@gmail.com>
2020-07-10 17:29:09 -0500

    updated data generation script

--- db/dataGeneration.js
+++ db/dataGeneration.js
@@ -4,61 +4,75 @@ const fs = require('fs');
 
 const numOfData = 10000000;
 
-const reviewEntry = () => ({
-  itemid: random.int(0, numOfData),
-  name: faker.name.findName(),
-  location: faker.fake('{{address.city}} {{address.stateAbbr}}'),
-  title: faker.fake('{{hacker.ingverb}} {{hacker.adjective}} {{hacker.noun}}'),
-  comment: faker.fake('{{hacker.phrase}} {{hacker.phrase}} {{hacker.phrase}}'),
+const reviewEntry = (id) => {
+  var array = [];
+  var randReviews = random.int(1, 10);
+  for (var i = 0; i < randReviews; i++) {
+    var reviewEntry = {
+      itemid: id,
+      name: faker.name.findName(),
+      location: faker.fake('{{address.city}} {{address.stateAbbr}}'),
+      title: faker.hacker.phrase(),
+      comment: faker.lorem.sentences(),
+      likes: random.int(0, 51),
+      dislike: random.int(0, 51),
+      star: random.int(0, 51),
+    };
+    array.push(reviewEntry);
+  }
+  return array;
+};
 
-  like: random.int(0, 51),
-  dislike: random.int(0, 51),
-  star: random.int(0, 51),
-});
-
-var writeReviews = fs
-  .createWriteStream(`reviews.csv`)
-  .on('finsh', () => console.log('Data generation finished'))
-  .on('error', (err) => console.log(err));
-
-var convertToCSV = (json) => {
+var convertToCSV = (array) => {
   var header = [
     'itemid',
     'name',
     'location',
     'title',
     'comment',
-    'like',
+    'likes',
     'dislike',
     'star',
   ];
   var csvline = '';
-  for (var i = 0; i < header.length; i++) {
-    var key = header[i];
-    if (i === header.length - 1) {
-      csvline += json[key];
-    } else {
-      csvline += json[key] + ',';
+  for (var j = 0; j < array.length; j++) {
+    var json = array[j];
+    for (var i = 0; i < header.length; i++) {
+      var key = header[i];
+      if (i === header.length - 1) {
+        csvline += json[key];
+      } else {
+        csvline += json[key] + ';';
+      }
     }
+    csvline += '\n';
   }
-  csvline += '\n';
+
   return csvline;
 };
 
-var numOfReviews = numOfData * 5;
+var writeReviews = fs
+  .createWriteStream(`reviews2.csv`, { flag: 'a' })
+  .on('finish', () => console.log('Data generation finished'))
+  .on('error', (err) => console.log(err));
 
-var i = 0;
+var i = 449700;
+var start = Date.now();
 write = () => {
   let ok = true;
-  while (i < numOfReviews && ok) {
-    data = convertToCSV(reviewEntry());
+  while (i < numOfData && ok) {
+    var data = '';
+    for (var j = i; j < i + 1000; j++) {
+      data += convertToCSV(reviewEntry(j));
+    }
     ok = writeReviews.write(data);
-    i++;
+    i += 1000;
   }
-  if (i < numOfReviews) {
+  if (i < numOfData) {
     writeReviews.once('drain', write);
   } else {
     writeReviews.end();
+    console.log(Date.now() - start);
   }
 };
 write();
