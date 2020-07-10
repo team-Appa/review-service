a9ddcacce78edad14fe7acce7d90bcd637efa0e0
Jing Chen <jchen0506@gmail.com>
2020-07-09 19:17:19 -0500

    data generation script ready

--- db/dataGeneration.js
+++ db/dataGeneration.js
@@ -1,13 +1,13 @@
 const faker = require('faker');
 const random = require('random');
+const fs = require('fs');
 
-//gloable variable for total data entry point
-const numOfData = 10;
-//number of reviews will be 5-10 times of num of data point
+const numOfData = 10000000;
+
 const reviewEntry = () => ({
   itemid: random.int(0, numOfData),
   name: faker.name.findName(),
-  location: faker.fake('{{address.city}} {{address.stateAbbr}}'),
+  location: faker.fake('{{address.city}}, {{address.stateAbbr}}'),
   title: faker.fake('{{hacker.ingverb}} {{hacker.adjective}} {{hacker.noun}}'),
   comment: faker.fake('{{hacker.phrase}} {{hacker.phrase}} {{hacker.phrase}}'),
 
@@ -16,31 +16,27 @@ const reviewEntry = () => ({
   star: random.int(0, 51),
 });
 
-var dataChunk = () => {
-  var chunk = [];
-  //numOfData is the number of reviews
-  for (var i = 0; i < numOfData * 10; i++) {
-    chunk.push(reviewEntry());
-  }
-  return chunk;
-};
+const writeReviews = fs.createWriteStream('reviews.csv');
 
-var convertCSV = (json) => {
-  var header = Object.keys(json[0]);
+function writeFiftyMillionTimes(writer, encoding, callback) {
+  let data = '';
+  let i = numOfData * 5;
+  write();
 
-  var csv = header.join(',') + '\n';
-
-  for (var i = 0; i < json.length; i++) {
-    var line = '';
-    for (var j = 0; j < header.length; j++) {
-      var key = header[j];
-      line += json[i][key] + ',';
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        data = JSON.stringify(reviewEntry()) + '\n';
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
     }
-    csv += line.slice(0, line.length - 1) + '\n';
   }
-  return csv;
-};
-var data = dataChunk();
-// console.log(data);
-
-console.log(convertCSV(data));
+}
+writeFiftyMillionTimes(writeReviews, 'utf8', () => writeReviews.end());
