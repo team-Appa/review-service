6d55b2da311cd0089e6481ca367ce735b2617730
Jing Chen <jchen0506@gmail.com>
2020-07-14 21:50:41 -0500

    clean up, prepared for using psql

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const mongoUri = 'mongodb://localhost/review';
 
-const db = mongoose.connect(mongoUri, {useMongoClient: true});
+const db = mongoose.connect(mongoUri, { useMongoClient: true });
 
 module.exports = db;
