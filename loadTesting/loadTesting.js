1f0512f99ff01db90e2076d5e99d39b3a3f8c8cb
Jing Chen <jchen0506@gmail.com>
2020-07-23 11:59:17 -0500

    modified stress testing code

--- loadTesting/loadTesting.js
+++ loadTesting/loadTesting.js
@@ -7,19 +7,20 @@ export let options = {
     { duration: '1m', target: 1 }, // below normal load
     { duration: '1m', target: 10 },
     { duration: '2m', target: 50 },
-    { duration: '2m', target: 50 }, // around the breaking point
-    // { duration: '5m', target: 200 }, // cloud testing won't allow more than 50 vus
+    { duration: '5m', target: 50 }, // around the breaking point
+    // { duration: '5m', target: 100 }, // around the breaking point
   ],
-  ext: {
-    loadimpact: {
-      projectID: 3500599,
-      // Test runs with the same name groups test runs together
-      name: 'Cloud Test with 50 vus maxed',
-    },
-  },
+  // ext: {
+  //   loadimpact: {
+  //     projectID: 3500599,
+  //     // Test runs with the same name groups test runs together
+  //     name: 'Cloud Test with 50 vus maxed',
+  //   },
+  // },
 };
 
 export default function () {
-  var rand = Math.floor(Math.random() * 10) + 9999000;
-  let responses = http.get(`http://54.144.251.21:3004/api/reviews/?id=${rand}`);
+  // var rand = Math.floor(Math.random() * 10) + 9999000;
+  var rand = Math.floor(Math.random() * 9999999);
+  let responses = http.get(`http://3.221.150.39/api/reviews?id=${rand}`);
 }
